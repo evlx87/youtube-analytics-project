95b3442f2a43c6c938d11e4a0595e339882a28bf
Sergey Semyonov <evlx@mail.ru>
2023-10-10 17:34:48 +0300

    add hw_2

--- src/channel.py
+++ src/channel.py
@@ -2,17 +2,50 @@ import json
 import os
 from googleapiclient.discovery import build
 
+api_key: str = os.getenv('YT_API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
+
 
 class Channel:
     """Класс для ютуб-канала"""
-    api_key: str = os.getenv('YT_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """
+        Экземпляр инициализируется id канала.
+        Дальше все данные будут подтягиваться по API.
+        """
         self.channel_id = channel_id
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        channel = youtube.channels().list(
+            id=self.channel_id,
+            part='snippet,statistics').execute()
+
+        self.title = channel['items'][0]['snippet']['title']
+        self.description = channel['items'][0]['snippet']['description']
+        self.url = 'https://www.youtube.com/' + channel['items'][0]['snippet']['customUrl']
+        self.subs_count = channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+        self.view_count = channel['items'][0]['statistics']['viewCount']
+
+    # def print_info(self) -> None:
+    #   """Выводит в консоль информацию о канале."""
+    #   channel = self.youtube.channels().list(
+    #       id=self.channel_id,
+    #       part='snippet,statistics').execute()
+    #   print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=api_key)
+
+    def to_json(self, file):
+        with open(file, 'wt') as file:
+            data = {'chanel_id': self.channel_id,
+                    'title': self.title,
+                    'description': self.description,
+                    'url': self.url,
+                    'subscribers_count': self.subs_count,
+                    'video_count': self.video_count,
+                    'view_count': self.view_count}
+            json_data = json.dumps(data, ensure_ascii=False, indent=2)
+            file.write(json_data)

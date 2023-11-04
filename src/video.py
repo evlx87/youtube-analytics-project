17ee01ccf475349d7afcb0c31cd7e628ec4a2c4c
Sergey Semyonov <evlx@mail.ru>
2023-11-04 21:40:59 +0300

    add hw_6

--- src/video.py
+++ src/video.py
@@ -1,4 +1,3 @@
-import json
 import os
 from googleapiclient.discovery import build
 
@@ -9,16 +8,21 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 class Video:
     def __init__(self, video_id: str):
-        self.__video_id = video_id  # id видео;
-        video_response = youtube.videos().list(id=self.__video_id,
-                                               part='snippet,statistics,contentDetails,topicDetails').execute()
-        self.video_title = video_response['items'][0]['snippet']['title']  # Название видео
-        self.url = 'https://youtu.be/' + video_id  # Ссылка на видео
-        self.view_count = video_response['items'][0]['statistics']['viewCount']  # Кол-во просмотров
-        self.count_likes = video_response['items'][0]['statistics']['likeCount']  # Кол-во лайков
+        self.video_id = video_id
+        try:
+            video_response = youtube.videos().list(id=self.video_id,
+                                                   part='snippet,statistics,contentDetails,topicDetails').execute()
+            self.url = 'https://youtu.be/' + video_id
+            self.title = video_response['items'][0]['snippet']['title']
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.title = None
+            self.like_count = None
+            self.view_count = None
 
     def __str__(self):
-        return self.video_title
+        return self.title
 
 
 class PLVideo:
@@ -26,10 +30,10 @@ class PLVideo:
         self.__video_id = video_id  # id видео;
         video_response = youtube.videos().list(id=self.__video_id,
                                                part='snippet,statistics,contentDetails,topicDetails').execute()
-        self.video_title = video_response['items'][0]['snippet']['title']  # Название видео
-        self.url = 'https://youtu.be/' + playlist_id + '/' + video_id  # Ссылка на видео
-        self.view_count = video_response['items'][0]['statistics']['viewCount']  # Кол-во просмотров
-        self.count_likes = video_response['items'][0]['statistics']['likeCount']  # Кол-во лайков
+        self.video_title = video_response['items'][0]['snippet']['title']
+        self.url = 'https://youtu.be/' + playlist_id + '/' + video_id
+        self.view_count = video_response['items'][0]['statistics']['viewCount']
+        self.count_likes = video_response['items'][0]['statistics']['likeCount']
         self.__playlist_id = playlist_id
 
     def __str__(self):
